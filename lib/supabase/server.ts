be98191ab1aaedcb6909b5d15e4e39badf41679f
Siddiq Achmad <siddiq.dizy@hotmail.com>
2025-08-03 22:58:26 +0700

    Fix : Middleware

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -8,6 +8,13 @@ export async function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
+      cookieOptions: {
+        name: "sb-luxima-auth-token",
+        domain: ".luxima.id",
+        secure: true,
+        path: "/",
+        sameSite: "lax",
+      },
       cookies: {
         getAll() {
           return cookieStore.getAll();
